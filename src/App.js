5cfc49ce4c02e4a8173761a31539124328c742b0
MHanSi Senanayake <39160365+mhansi@users.noreply.github.com>
2020-06-12 00:24:11 +0530

    Added some features.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
+
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.compnent';
 
 class App extends Component{
  render(){
    return(
+    <Router>
      <div className="container"> 
-     <h2>React CRUD</h2>
+      <nav className = "navbar navbar-brand navbar-light bg-light" >
+           <Link to={'/'} className="navbar-brand">React CRUD</Link>
+        
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={'/'} className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={'/create'} className="nav-link">Create</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={'/index'} className="nav-link">Index</Link>
+            </li>
+          </ul>
+        
+      </nav>
+      <br/>
+      <h2>Welcome to REACT CRUD</h2><br/>
+      <Switch>
+        <Route exact path='/create' component={Create}/>
+        <Route exact path = '/edit/:id' component={Edit}/>
+        <Route exact path='/index' component={Index}/>
+       
+      </Switch>
      </div>
+    </Router>
    );
  }
 }
---
+++ src/components/create.component.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class Create extends Component{
+    render(){
+        return(
+            <div>
+                <p>Welcome to Create compnent</p>
+            </div>
+        )
+    }
+}
